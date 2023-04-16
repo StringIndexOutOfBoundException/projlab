2c0837f1109e072d85ab4973bacc5a1cac77eda0
saviourdavid <SaviourDavid420@gmail.com>
2023-04-17 00:39:25 +0200

    Csak kiiratas

--- src/main/java/projlab/UseCase10.java
+++ src/main/java/projlab/UseCase10.java
@@ -27,11 +27,9 @@ public class UseCase10 {
 		
 		//most van egy "cs" csovunk, aminek van ket pumpaja "p" es "p1", illetve egy szerelo,
 		// aki a "cs" csovon all es van egy pumpa "ujPumpa" a hatizsakjaban
-		//System.out.println("Letrejott: cs:Cso,p:Pumpa,p1:Pumpa,sz:Szerelo,ujPumpa:Pumpa");
-		
 
-		//System.out.println("Fuggvenyhivas:sz.PumpatEpit()");
-		//System.out.println("Fuggvenyhivas:cs.PumpaEpit()");
+		System.out.println("\n\nFuggvenyhivas:sz.PumpatEpit()");
+		System.out.println("Fuggvenyhivas:cs.PumpaEpit()");
 		sz.PumpatEpit();
 	
 	
