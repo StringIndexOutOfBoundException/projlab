c1fcd965fcf59dbb1faf4ff78dcd5b04c04ab3a7
saviourdavid <SaviourDavid420@gmail.com>
2023-05-13 20:48:21 +0200

    format

--- src/main/java/projlab/Ciszterna.java
+++ src/main/java/projlab/Ciszterna.java
@@ -6,64 +6,65 @@ import java.util.Random;
 import java.util.ArrayList;
 
 /**
- * A Forrás a pálya aktív eleme.
- * Azért felelős, hogy vizet nyeljen el, amit a csőhálózaton keresztűl kap.
- * Továbbá Pumpákat és Csöveket termel.
- * Összesen maximum 20 pumpája és hozzácsatlakozott csöve lehet egy csiszternának
+ * A Forrás a pálya aktív eleme. Azért felelős, hogy vizet nyeljen el, amit a
+ * csőhálózaton keresztűl kap. Továbbá Pumpákat és Csöveket termel. Összesen
+ * maximum 20 pumpája és hozzácsatlakozott csöve lehet egy csiszternának
  */
 public class Ciszterna extends Mezo {
-	
+
 	private ArrayList<Mezo> termeltpumpak;
-	
+
 	public Ciszterna() {
 		super(Integer.MAX_VALUE);
-		termeltpumpak=new ArrayList<Mezo>();
-		maxSzomszedok=20;
+		termeltpumpak = new ArrayList<Mezo>();
+		maxSzomszedok = 20;
 	}
-	
+
 	/**
-	 *Ha a termeltpumpák elemszáma 0, akkor visszajelzést ad, hogy nincs felvehető pumpa. 
-	 *Ha a termeltpumpák elemszáma nagyobb mint 0, akkor az listaban utolso pumpaja eltávolításra kerül.
+	 * Ha a termeltpumpák elemszáma 0, akkor visszajelzést ad, hogy nincs felvehető
+	 * pumpa. Ha a termeltpumpák elemszáma nagyobb mint 0, akkor az listaban utolso
+	 * pumpaja eltávolításra kerül.
 	 */
 	public void PumpaEltavolit() {
-		//ha van pumpa a ciszterna korul
-		if(termeltpumpak.size()>=1)
-		termeltpumpak.remove(termeltpumpak.size()-1);
-		}
-	
+		// ha van pumpa a ciszterna korul
+		if (termeltpumpak.size() >= 1)
+			termeltpumpak.remove(termeltpumpak.size() - 1);
+	}
+
 	/**
-	 * A ciszternak ezzel a fuggvennyel keszitenek uj pumpakat minden hivas utan 0-2 szam intervallumban
+	 * A ciszternak ezzel a fuggvennyel keszitenek uj pumpakat minden hivas utan 0-2
+	 * szam intervallumban
 	 */
 	public void PumpaKeszit() {
-		//Random darab uj pumpat rak bele a termeltpumpakba 0-2 kozott
-		Random rand=new Random();
-		int randomNum=0;
-		if(doRandomThings) //ha determinisztikus a mukodes
-		randomNum=rand.nextInt(3);
-		for(int i=0; i<randomNum;++i) {
-			Pumpa p=new Pumpa();
+		// Random darab uj pumpat rak bele a termeltpumpakba 0-2 kozott
+		Random rand = new Random();
+		int randomNum = 0;
+		if (doRandomThings) // ha determinisztikus a mukodes
+			randomNum = rand.nextInt(3);
+		for (int i = 0; i < randomNum; ++i) {
+			Pumpa p = new Pumpa();
 			termeltpumpak.add(p);
 			p.SzomszedHozzaad(this);
 			this.SzomszedHozzaad(p);
 		}
-	
+
 	}
 
 	/**
 	 * A ciszterna egy magához kapcsolódó szabad végű csövet hoz létre.
 	 */
 	public void CsovetKeszit() {
-		Random rand=new Random();
-		int randomNum=0;
-		if(doRandomThings) //ha determinisztikus a mukodes
-		randomNum=rand.nextInt(3);
-		for(int i=0; i<randomNum;++i) {
+		Random rand = new Random();
+		int randomNum = 0;
+		if (doRandomThings) // ha determinisztikus a mukodes
+			randomNum = rand.nextInt(3);
+		for (int i = 0; i < randomNum; ++i) {
 			Cso ujcso = new Cso();
 			ujcso.SzomszedHozzaad(this);
 			this.SzomszedHozzaad(ujcso);
 		}
-		}
-		
+	}
+
 	/**
 	 * getter a termeltpumpakra
 	 */
@@ -72,18 +73,20 @@ public class Ciszterna extends Mezo {
 	}
 
 	/**
-	*A szomszédok listájában mindenkire meghívja a VizetCsokkent függvényt. Azaz minden rá csatlakoztatott csőtől elveszi a benne található vizet.
-	*(ha nincs víz a csőben, nem kap vizet)
-	*Meghívja a PumpatKeszit, CsovetKeszit függvényeket is. 
-	*@Override
-	*/
+	 * A szomszédok listájában mindenkire meghívja a VizetCsokkent függvényt. Azaz
+	 * minden rá csatlakoztatott csőtől elveszi a benne található vizet. (ha nincs
+	 * víz a csőben, nem kap vizet) Meghívja a PumpatKeszit, CsovetKeszit
+	 * függvényeket is.
+	 * 
+	 * @Override
+	 */
 	public void Frissit() {
-		ArrayList<Mezo> szomszedok = GetSzomszedok(); //GetLeszedhetoSzomszedok lesz
+		ArrayList<Mezo> szomszedok = GetSzomszedok(); // GetLeszedhetoSzomszedok lesz
 		for (var cso : szomszedok) {
 			try {
-				cso.VizetCsokkent(1);		
-			}
-			catch (Exception e){/* Hmm ez nem szép itt ;) */}
+				cso.VizetCsokkent(1);
+			} catch (Exception e) {
+				/* Hmm ez nem szép itt ;) */}
 		}
 		this.CsovetKeszit();
 		this.PumpaKeszit();
--- src/main/java/projlab/Pumpa.java
+++ src/main/java/projlab/Pumpa.java
@@ -5,66 +5,74 @@ import java.nio.BufferUnderflowException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+
 /**
-*A pumpák a víz folyását szabályozzák. Minden pumpához adott számú csövet(maxCso) lehet csatlakoztatni,
-*de ezekből mindig 1 kiválasztott cső a bemeneti cső(bemenet), amiből a pumpa magába tud szívni vizet 
-*és mindig 1 cső a kimeneti cső(kimenet),amibe a pumpa vizet tud pumpálni.
-*A pumpák véletlen időközönként elromlanak. Ilyenkor se vizet nem tudnak magukba szívni, se kipumpálni.
-*A pumpákban található víz (vízmennyiség) akkor nő meg ha bemenetéről szívja a vizet,
-*illetve akkor csökken, ha kimenetére kipumpálja a vizet (MAXVIZ attribútumnyit szív és kipumpál).
-*Maximum 5 egységnyi víz lehet egy pumpában
-*Ha a pumpa nem tud vizet átadni a kimenetére (mert a kimenet tele van vízzel),
-*akkor a víz a pumpában marad. A pumpa csak akkor tud vizet szívni és kipumpálni, ha működik(Mezo ososztaly mukodik attributuma). 
-*A pumpálásnak van felsőkorlátja(MAXVIZ), maximum ennyi vizet szívhat, illetve kipumpálhat a csövekből.
-*   
-*/
+ * A pumpák a víz folyását szabályozzák. Minden pumpához adott számú
+ * csövet(maxCso) lehet csatlakoztatni, de ezekből mindig 1 kiválasztott cső a
+ * bemeneti cső(bemenet), amiből a pumpa magába tud szívni vizet és mindig 1 cső
+ * a kimeneti cső(kimenet),amibe a pumpa vizet tud pumpálni. A pumpák véletlen
+ * időközönként elromlanak. Ilyenkor se vizet nem tudnak magukba szívni, se
+ * kipumpálni. A pumpákban található víz (vízmennyiség) akkor nő meg ha
+ * bemenetéről szívja a vizet, illetve akkor csökken, ha kimenetére kipumpálja a
+ * vizet (MAXVIZ attribútumnyit szív és kipumpál). Maximum 5 egységnyi víz lehet
+ * egy pumpában Ha a pumpa nem tud vizet átadni a kimenetére (mert a kimenet
+ * tele van vízzel), akkor a víz a pumpában marad. A pumpa csak akkor tud vizet
+ * szívni és kipumpálni, ha működik(Mezo ososztaly mukodik attributuma). A
+ * pumpálásnak van felsőkorlátja(MAXVIZ), maximum ennyi vizet szívhat, illetve
+ * kipumpálhat a csövekből.
+ * 
+ */
 public class Pumpa extends Mezo {
 
-	private  static int MAXVIZ;
+	private static int MAXVIZ;
 	private int maxCso;
 	private int vizmennyiseg;
 	private Mezo bemenet;
 	private Mezo kimenet;
-	
-	public Pumpa(){
+
+	public Pumpa() {
 		super(Integer.MAX_VALUE);
-		MAXVIZ=1;
-		maxCso=5;
-		vizmennyiseg=0;
-		bemenet=null;
-		kimenet=null;
+		MAXVIZ = 1;
+		maxCso = 5;
+		vizmennyiseg = 0;
+		bemenet = null;
+		kimenet = null;
 	}
+
 	/**
-	 * Egy mező szomszédaihoz hozzáad egy új mezőt
-	 * A maxCso attribútum határozza meg mennyi szomszédja(rácsatlakotatt csövek) lehet egy pumpának.
-	 * A szomszédok listáját a Mezo ősosztályból örökli.
+	 * Egy mező szomszédaihoz hozzáad egy új mezőt A maxCso attribútum határozza meg
+	 * mennyi szomszédja(rácsatlakotatt csövek) lehet egy pumpának. A szomszédok
+	 * listáját a Mezo ősosztályból örökli.
+	 * 
 	 * @param m a hozzáadandó mező
 	 * @Override
 	 */
 	public void SzomszedHozzaad(Mezo m) {
-		ArrayList<Mezo> szomszedok = super.GetSzomszedok(); //Mezo osztaly attributuma
-		if(szomszedok.size() < maxCso)
+		ArrayList<Mezo> szomszedok = super.GetSzomszedok(); // Mezo osztaly attributuma
+		if (szomszedok.size() < maxCso)
 			szomszedok.add(m);
 	}
 
 	/**
-	 *Egy játékos átállítja egy pumpa be és kimenetét.
-	 *Ha valamelyik paraméterben kapott referencia nem szerepel a pumpa szomszédai között, nem történik meg az átállítás
-	 *Ha a paraméterek szerepelnek a szomszédok között,akkor a pumpa bemenetét átállítja a paraméterként kapott bemenetre
-	 *és a kimenetét is a paraméterben kapott kimenetre állítja. 
+	 * Egy játékos átállítja egy pumpa be és kimenetét. Ha valamelyik paraméterben
+	 * kapott referencia nem szerepel a pumpa szomszédai között, nem történik meg az
+	 * átállítás Ha a paraméterek szerepelnek a szomszédok között,akkor a pumpa
+	 * bemenetét átállítja a paraméterként kapott bemenetre és a kimenetét is a
+	 * paraméterben kapott kimenetre állítja.
+	 * 
 	 * @param kimenet amire át lesz állítva a kimenet
 	 * @param bemenet amire át lesz állítva a bemenet
 	 * @Override
 	 */
 
 	public void Atallit(Mezo kimenet, Mezo bemenet) {
-		//megnezzuk szomszedok-e
-		ArrayList<Mezo> szomszedok = GetSzomszedok(); //Mezo osztaly attributuma
-		for(int i=0; i<szomszedok.size();++i) {
-			if(bemenet==szomszedok.get(i)) {
+		// megnezzuk szomszedok-e
+		ArrayList<Mezo> szomszedok = GetSzomszedok(); // Mezo osztaly attributuma
+		for (int i = 0; i < szomszedok.size(); ++i) {
+			if (bemenet == szomszedok.get(i)) {
 				this.bemenet = bemenet;
 			}
-			if(kimenet==szomszedok.get(i)) {
+			if (kimenet == szomszedok.get(i)) {
 				this.kimenet = kimenet;
 			}
 		}
@@ -72,6 +80,7 @@ public class Pumpa extends Mezo {
 
 	/**
 	 * Átállítja a mukodik attributumot true-ra.
+	 * 
 	 * @Override
 	 */
 
@@ -80,87 +89,92 @@ public class Pumpa extends Mezo {
 	}
 
 	/**
-	 *Felüldefiniált metódus a Mezo osztályból.
-	 *A pumpa e függvény hatására (determinisztikusan vagy véletlenszerűen (az aktivitásdiagrammon 50% esély van erre))
-	 *elromlik (mukodik attributum értékét false-ra állítja), ekkor nem tud vizet szívni, kipumpálni. 
-	 *Ha a pumpa működik (mukodik attributum értéke true) a bemenetéről először beszívja a vizet a tartályába
-	 *(ha van víz maximum egy egységnyivel nő a vizmennyiseg attribútum értéke, mivel a csövek maximum egy egységnyi vizet tárolhatnak).
-	 *Ezután kipumpálja a vizet a kimenetére (ha van víz akkor maximum eggyel csökken a vízmennyiségattribútum).
-	 *Ha a kimenet tele van vízzel, akkor a tartályban marad a víz.
-	 *Ha a bemenetéről nem tud vizet szívni, akkor a tartályában lévő vizet adja át a kimenetnek (ha a tartályban nincs víz nem kap a kimenet vizet) 
+	 * Felüldefiniált metódus a Mezo osztályból. A pumpa e függvény hatására
+	 * (determinisztikusan vagy véletlenszerűen (az aktivitásdiagrammon 50% esély
+	 * van erre)) elromlik (mukodik attributum értékét false-ra állítja), ekkor nem
+	 * tud vizet szívni, kipumpálni. Ha a pumpa működik (mukodik attributum értéke
+	 * true) a bemenetéről először beszívja a vizet a tartályába (ha van víz maximum
+	 * egy egységnyivel nő a vizmennyiseg attribútum értéke, mivel a csövek maximum
+	 * egy egységnyi vizet tárolhatnak). Ezután kipumpálja a vizet a kimenetére (ha
+	 * van víz akkor maximum eggyel csökken a vízmennyiségattribútum). Ha a kimenet
+	 * tele van vízzel, akkor a tartályban marad a víz. Ha a bemenetéről nem tud
+	 * vizet szívni, akkor a tartályában lévő vizet adja át a kimenetnek (ha a
+	 * tartályban nincs víz nem kap a kimenet vizet)
+	 * 
 	 * @Override
 	 */
 	public void Frissit() {
 		Random rand = new Random();
-		if(rand.nextDouble(1) > 0.5){
-			if(doRandomThings) //ha determinisztikus a mukodes
-			this.setMukodik(false);
+		if (rand.nextDouble(1) > 0.5) {
+			if (doRandomThings) // ha determinisztikus a mukodes
+				this.setMukodik(false);
 		}
-		//ha mukodik akkor pumpalhat
-		if(this.mukodik) {
-		//ha a pumpa tartalya ures vagy nincs tele	
-		if(vizmennyiseg<5) {
-				if(bemenet!=null) {
-					int vizet_sziv=bemenet.getVizmennyiseg(); //annyi vizet sziv ki amennyi a bemenetben van
+		// ha mukodik akkor pumpalhat
+		if (this.mukodik) {
+			// ha a pumpa tartalya ures vagy nincs tele
+			if (vizmennyiseg < 5) {
+				if (bemenet != null) {
+					int vizet_sziv = bemenet.getVizmennyiseg(); // annyi vizet sziv ki amennyi a bemenetben van
 					bemenet.VizetCsokkent(vizet_sziv);
-					vizmennyiseg+=vizet_sziv;
+					vizmennyiseg += vizet_sziv;
 				}
-			//vizet átad kimenetnek, ha van viz
-			if(vizmennyiseg>=1) {	
-				int vizet_pumpal=MAXVIZ-kimenet.getVizmennyiseg(); //MAXVIZ=1, ennyi fér bele egy csőbe
-				kimenet.VizetNovel(vizet_pumpal);
-				vizmennyiseg-=vizet_pumpal;
-		}
-		}
+				// vizet átad kimenetnek, ha van viz
+				if (vizmennyiseg >= 1) {
+					int vizet_pumpal = MAXVIZ - kimenet.getVizmennyiseg(); // MAXVIZ=1, ennyi fér bele egy csőbe
+					kimenet.VizetNovel(vizet_pumpal);
+					vizmennyiseg -= vizet_pumpal;
+				}
+			}
 
-		//ha tele van a pumpa
-		 if(vizmennyiseg==5) {
-			
-			//eloszor megprobal kimenetre kipumpalni
-				int vizet_pumpal=MAXVIZ-kimenet.getVizmennyiseg();
+			// ha tele van a pumpa
+			if (vizmennyiseg == 5) {
+
+				// eloszor megprobal kimenetre kipumpalni
+				int vizet_pumpal = MAXVIZ - kimenet.getVizmennyiseg();
 				kimenet.VizetNovel(vizet_pumpal);
-				vizmennyiseg-=vizet_pumpal;		
-			
-			//ha sikerult atadni a vizet a kimenetnek a bemenettol kaphat vizet
-			 if(vizmennyiseg!=5) {
-				if(bemenet!=null) {
-					int vizet_sziv=bemenet.getVizmennyiseg();
-					//Ha tul tolteni a pumpat a bementrol kapott viz, akkor csak annyit toltunk be amennyi a pumpaba befer
-					if(vizmennyiseg+vizet_sziv>5)
-					vizet_sziv=5-vizmennyiseg;
-					bemenet.VizetCsokkent(vizet_sziv);
-					vizmennyiseg+=vizet_sziv;
-				}
+				vizmennyiseg -= vizet_pumpal;
+
+				// ha sikerult atadni a vizet a kimenetnek a bemenettol kaphat vizet
+				if (vizmennyiseg != 5) {
+					if (bemenet != null) {
+						int vizet_sziv = bemenet.getVizmennyiseg();
+						// Ha tul tolteni a pumpat a bementrol kapott viz, akkor csak annyit toltunk be
+						// amennyi a pumpaba befer
+						if (vizmennyiseg + vizet_sziv > 5)
+							vizet_sziv = 5 - vizmennyiseg;
+						bemenet.VizetCsokkent(vizet_sziv);
+						vizmennyiseg += vizet_sziv;
+					}
 				}
 			}
 		}
-		}
-	
+	}
 
 	/**
 	 * getter a bemenetre
 	 */
 	public Mezo getBemenet() {
-	return this.bemenet;
-}
-	/**
-	 * getter a Kimenetre
-	 * Hasznalva van
-	 */
-public Mezo getKimenet() {
-	return this.kimenet;
-}
-/**
- * Mennyi víz legyen a pumpában (max 5 lehet es 0-nal tobb)
- * @param meret: viz mennyisege
- */
-public void setVizmennyiseg(int meret) {
-	//max 5 viz lehet benne
-	if(meret>5) {
-		this.vizmennyiseg=5;
+		return this.bemenet;
 	}
-	else if(meret<0) {}
-	else
-	this.vizmennyiseg=meret;
-}
+
+	/**
+	 * getter a Kimenetre Hasznalva van
+	 */
+	public Mezo getKimenet() {
+		return this.kimenet;
+	}
+
+	/**
+	 * Mennyi víz legyen a pumpában (max 5 lehet es 0-nal tobb)
+	 * 
+	 * @param meret: viz mennyisege
+	 */
+	public void setVizmennyiseg(int meret) {
+		// max 5 viz lehet benne
+		if (meret > 5) {
+			this.vizmennyiseg = 5;
+		} else if (meret < 0) {
+		} else
+			this.vizmennyiseg = meret;
+	}
 }
