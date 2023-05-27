782bf141cd17bcdd3d6f0cbb4a32e3affc23c7f8
tsgrgo <tasigergo6@gmail.com>
2023-05-28 00:39:02 +0200

    Create CsoView.java

---
+++ src/main/java/projlab/CsoView.java
@@ -0,0 +1,19 @@
+package projlab;
+
+import java.awt.Graphics;
+
+public class CsoView extends ObjectView {
+
+	@Override
+	public void Notify(Mezo m) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void Draw(Graphics g) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
