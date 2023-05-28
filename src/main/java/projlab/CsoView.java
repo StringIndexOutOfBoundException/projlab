ce64433027f68428843b1aa7753a7ca3987f74b1
tsgrgo <tasigergo6@gmail.com>
2023-05-28 05:38:37 +0200

    Update CsoView.java

--- src/main/java/projlab/CsoView.java
+++ src/main/java/projlab/CsoView.java
@@ -8,7 +8,8 @@ import java.util.ArrayList;
 
 public class CsoView extends ObjectView {
 	private static final Color szin = Color.BLACK; // Fekete
-	private static final BasicStroke vastagsag = new BasicStroke(10); // Kirajzolt cső vastagsága
+	private static final int vastag = 10; // Kirajzolt cső vastagsága
+	private static final BasicStroke stroke = new BasicStroke(vastag);
 
 	/**
 	 * A cső egyik vége az ObjectView-ből örökölt x, y. A cső másik vége pedig ez a
@@ -16,7 +17,13 @@ public class CsoView extends ObjectView {
 	 */
 	private int x2, y2;
 
+	/**
+	 * Cső állapotát jelentő változók (keep for now)
+	 */
 	private Boolean visible = false;
+	private Boolean csuszik = false;
+	private Boolean ragad = false;
+	private Boolean mukodik = true;
 
 	/**
 	 * Amikor a cső változott, akkor átadja magát a hozzá tartozó view-nak, ami ez
@@ -43,6 +50,11 @@ public class CsoView extends ObjectView {
 		y = v1.y;
 		x2 = v2.x;
 		y2 = v2.y;
+
+		// Cső modifierek
+		csuszik = m.getCsuszos() > 0;
+		ragad = m.getRagados() > 0;
+		mukodik = m.getMukodik();
 	}
 
 	/**
@@ -58,10 +70,29 @@ public class CsoView extends ObjectView {
 		Graphics g = layers.get(0);
 		Graphics2D g2 = (Graphics2D) g;
 
-		g2.setStroke(vastagsag);
+		// Modifierek
+		if (csuszik || ragad) {
+			g2.setStroke(
+					new BasicStroke(vastag + 4, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[] { 9 }, 0));
+			// Csuszósságnak nagyobb a precedenciája -- ez alapján dől el a modifier szine
+			g2.setColor(csuszik ? Color.CYAN : Color.GREEN);
+			g2.drawLine(x, y, x2, y2);
+		}
+
+		// Cső rajzolása
+		g2.setStroke(stroke);
 		g2.setColor(szin);
 		g2.drawLine(x, y, x2, y2);
 
+		if (!mukodik) {
+			g2.setStroke(
+					new BasicStroke(vastag, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[] { 3 }, 0));
+			// Csuszósságnak nagyobb a precedenciája -- ez alapján dől el a modifier szine
+			g2.setColor(new Color(255, 0, 0, 100));
+			g2.drawLine(x, y, x2, y2);
+		}
+
+		// Név a cső közepén
 		DrawName(g, (x + x2) / 2, (y + y2) / 2);
 	}
 
